bcf632e0fbc0d4f9055f4850cbb81d58eede6267
Uzair Khan <uzairhk16@gmail.com>
2024-09-09 09:07:45 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -42,62 +42,63 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <>
-      <div
-        className="w-full max-w-md mx-auto shadow-md
+        <div
+          className="w-full max-w-md mx-auto shadow-md
       rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700"
-      >
-        <h1 className="text-white text-center my-3">Password Generator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3"
-            placeholder="password"
-            ref={passwordRef}
-          />
-          <button onClick={copyPasswordToClipboard}
-            className="outline-none px-3 py-0.5 shrink-0 
+        >
+          <h1 className="text-white text-center my-3">Password Generator</h1>
+          <div className="flex shadow rounded-lg overflow-hidden mb-4">
+            <input
+              type="text"
+              value={password}
+              className="outline-none w-full py-1 px-3"
+              placeholder="password"
+              ref={passwordRef}
+            />
+            <button onClick={copyPasswordToClipboard}
+              className="outline-none px-3 py-0.5 shrink-0 
           text-white bg-blue-500"
-          >
-            Copy
-          </button>
+            >
+              Copy
+              <i className="mx-2 fa-regular fa-copy"></i>
+            </button>
+          </div>
+          <div className="text-sm flex gap-x-2">
+            <div className="flex gap-x-1 items-center">
+              <input
+                type="range"
+                min={6}
+                max={50}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => { setLength(e.target.value) }}
+              />
+              <label > Length = {length}</label>
+            </div>
+            <div className="flex gap-x-1 items-center">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="numberInput">Numbers</label>
+            </div>
+            <div className="flex gap-x-1 items-center">
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="characterInput"
+                onChange={() => {
+                  setCharALlowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+            </div>
+          </div>
         </div>
-        <div className="text-sm flex gap-x-2">
-          <div className="flex gap-x-1 items-center">
-            <input
-              type="range"
-              min={6}
-              max={50}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => { setLength(e.target.value) }}
-            />
-            <label > Length = {length}</label>
-          </div>
-          <div className="flex gap-x-1 items-center">
-            <input
-              type="checkbox"
-              defaultChecked={numberAllowed}
-              id="numberInput"
-              onChange={() => {
-                setNumberAllowed((prev) => !prev);
-              }}
-            />
-            <label htmlFor="numberInput">Numbers</label>
-          </div>
-          <div className="flex gap-x-1 items-center">
-            <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="characterInput"
-              onChange={() => {
-                setCharALlowed((prev) => !prev);
-              }}
-            />
-            <label htmlFor="characterInput">Characters</label>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
