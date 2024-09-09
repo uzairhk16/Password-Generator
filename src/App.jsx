9812e308752201ceaec0c74ff80362b5b338e9b1
Uzair Khan <uzairhk16@gmail.com>
2024-09-09 08:42:53 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharALlowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  // useRef hook - used to give reference of selected components in our page so that 
+  // functions can be performed on referenced values
+
+  const passwordRef = useRef(null)
+
+  // useCallback hook -  used for optimization, it calls the function inside it 
+  // when the dependencies are changed and returns a memorized function 
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -15,11 +23,23 @@ function App() {
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 20);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  // useEffect hook -  runs the function inside it whenever the
+  //  page renders for the first-time or dependencies are changed
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <>
       <div
@@ -33,9 +53,9 @@ function App() {
             value={password}
             className="outline-none w-full py-1 px-3"
             placeholder="password"
-            readOnly
+            ref={passwordRef}
           />
-          <button
+          <button onClick={copyPasswordToClipboard}
             className="outline-none px-3 py-0.5 shrink-0 
           text-white bg-blue-500"
           >
@@ -44,18 +64,37 @@ function App() {
         </div>
         <div className="text-sm flex gap-x-2">
           <div className="flex gap-x-1 items-center">
-            <input 
-            type="range" 
-            min={6}
-            max={50}
-            value={length}
-            className="cursor-pointer"
-            onChange={(e) => {setLength(e.target.value)}}
+            <input
+              type="range"
+              min={6}
+              max={50}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => { setLength(e.target.value) }}
             />
             <label > Length = {length}</label>
-          </div> 
+          </div>
           <div className="flex gap-x-1 items-center">
-            
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex gap-x-1 items-center">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharALlowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
           </div>
         </div>
       </div>
